902e56791ba5573cca5cf63f203fd148eb506c03
K3N <github@epistemex.com>
2017-09-30 18:06:30 -0500

    See Change.log

--- README.md
+++ README.md
@@ -1,18 +1,24 @@
 ﻿canvas-mouse
-======
+============
 
-Helper object for getting the correct mouse or touch position in 2D canvas efficiently
+Helper object to efficiently obtain the correct mouse or touch position in 2D canvas
+independent of element size versus bitmap size, transforms, borders and padding. 
+
+Useful from simple cases where you need to adjust the position to match canvas
+in a web page, to more complex cases such as click-to-zoom, pan, rotate and 
+scale scenarios.
 
 
 Features
 --------
 
-- Efficient, causes no unneeded DOM reflows (handles scrolling and resizing gracefully)
-- Handles scaled canvas element (CSS) versus bitmap size
+- Efficient, causes no unneeded DOM reflows (caches key-values and handles scrolling and resizing gracefully)
+- Handles CSS-scaled canvas element versus its bitmap size
 - Handles transformed context
 - Handles element padding and borders if any
-- Supports the [transformation-matrix-2d](https://github.com/epistemex/transformation-matrix-js) solution for browsers without
-native support for [`currentTransform`](https://devdocs.io/dom/canvasrenderingcontext2d/currenttransform).
+- Handling-methods can be changed in real-time (to optimize handling).
+- Supports the [transformation-matrix-2d](https://github.com/epistemex/transformation-matrix-js) solution (included) 
+for browsers without native support for [`currentTransform`](https://devdocs.io/dom/canvasrenderingcontext2d/currenttransform).
 
 
 Install
@@ -22,8 +28,7 @@ Install
 
 - Git using HTTPS: `git clone https://gitlab.com/epistemex/canvas-mouse.git`
 - Git using SSH: `git clone git@gitlab.com:epistemex/canvas-mouse.git`
-- Download [zip archive](https://gitlab.com/epistemex/canvas-mouse/repository/archive.zip?ref=master) and extract.
-- Download [tar ball](https://gitlab.com/epistemex/canvas-mouse/repository/archive.tar.gz?ref=master) and extract.
+- See [releases](https://github.com/epistemex/canvas-mouse/releases) for other options.
 
 	
 Usage
@@ -48,11 +53,24 @@ Then in the handler you're using pass the event to get the correct position:
 Issues
 ------
 
-- `currentTransform` is not supported by all browsers at the moment.
-A [custom solution](https://github.com/epistemex/transformation-matrix-js)
-can be used in place that is compatible will all browsers.
+Using mouse/touch positions with a transformed context:
 
-See the [issue tracker](https://gitlab.com/epistemex/canvas-mouse/issues) for details.
+The `currentTransform` is not supported by all browsers at the moment.
+Our [custom matrix solution](https://github.com/epistemex/transformation-matrix-js)
+can be used in place and is compatible will all browsers.
+
+Firefox is currently the only browser that support the property (but in 
+prefixed form) but returns an Array instead of a DOMMatrix. Chrome will 
+return a SVGMatrix, and only if experimental canvas features is enabled 
+behind flags.
+
+We recommend therefor using the custom matrix solution as this will be 
+cross-platform compatible and independent of browser's current status.
+
+The only drawback is that you'll call transform operations on the matrix 
+object instead of the context (the matrix will automatically sync the 
+context for you though). See included demo (`www/demo_custommatrix.html`)
+for an usage example.
 
 
 License
--- src/canvas-mouse.js
+++ src/canvas-mouse.js
@@ -1,10 +1,263 @@
 /*!
-	canvas-mouse v. 0.5.0 alpha
-	Copyright (c) 2017 Epistemex.com
+	canvas-mouse 0.6.0 alpha
+	Copyright (c) 2017 Epistemex
 	MIT license.
 */
 
 ;"use strict";
+
+/**
+ * A mouse and touch position handler for 2D canvas able to handle scaled
+ * and transformed context and element.
+ *
+ * @param {CanvasRenderingContext2D} context - 2D context to bind to this instance
+ * @param {CanvasMouseOptions} [options] - optional options object to allow for scale and transforms to be considered.
+ * @constructor
+ */
+function CanvasMouse(context, options) {
+
+  if (!(context instanceof CanvasRenderingContext2D))
+    throw "Need a 2D canvas context.";
+
+  options = Object.assign({
+    handleScale: false,
+    handleTransforms: false,
+    matrix: null
+  }, options);
+
+  var
+    me = this,
+    defProp = Object.defineProperty.bind(Object),
+    ref,
+    hasCurrentTransform = ("currentTransform" in context || "mozCurrentTransform" in context),
+    deltaX,
+    deltaY,
+    scaleX,
+    scaleY,
+    paddingBorderLeft,
+    paddingBorderTop,
+    canvas = context.canvas,
+    matrix = options.matrix,
+    imatrix = matrix ? matrix.inverse() : null,
+    doScale = options.handleScale,
+    doTransforms = options.handleTransforms && (hasCurrentTransform || matrix),
+    transform = matrix ? _transforms2 : _transforms;
+
+  /*--------------------------------------------------------------------
+
+      INTERNALS
+
+  --------------------------------------------------------------------*/
+
+  if (doTransforms && !hasCurrentTransform && !matrix) {
+    console.log("Browser doesn't support currentTransform.");
+    doTransforms = false;
+  }
+
+  // optimize inverse matrix extraction
+  function patchMatrix() {
+    if (matrix && !matrix._xx) {
+      matrix._xx = matrix._x;
+      matrix._x = function() {
+        imatrix = this.inverse();
+        return this._xx();
+      }
+    }
+  }
+
+  // Limit reflow so only read trigger values when absolutely needed
+  function init() {
+    var
+      rect = canvas.getBoundingClientRect(),
+      cs = getComputedStyle(canvas),
+      prop = cs.getPropertyValue.bind(cs),
+      pInt = parseInt,
+      _p = "padding-", _b = "border-", _w = "-width",
+      L = "left", R = "right", T = "top", B = "bottom";
+
+    paddingBorderLeft = pInt(prop(_p + L)) + pInt(prop(_b + L + _w));
+    paddingBorderTop = pInt(prop(_p + T)) + pInt(prop(_b + T + _w));
+    deltaX = rect.left + paddingBorderLeft;
+    deltaY = rect.top + paddingBorderTop;
+    scaleX = (canvas.width / (rect.width - (paddingBorderLeft + pInt(prop(_p + R)) + pInt(prop(_b + R + _w))))) || 1;
+    scaleY = (canvas.height / (rect.height - (paddingBorderTop + pInt(prop(_p + B)) + pInt(prop(_b + B + _w))))) || 1;
+  }
+
+  // debounce updates
+  function _handler() {
+    cancelAnimationFrame(ref);
+    ref = requestAnimationFrame(_updateOnScroll)
+  }
+
+  function _updateOnScroll() {
+    var rect = canvas.getBoundingClientRect();
+    deltaX = rect.left + paddingBorderLeft;
+    deltaY = rect.top + paddingBorderTop;
+  }
+
+  /*--------------------------------------------------------------------
+
+      POINT CONVERTERS
+
+  --------------------------------------------------------------------*/
+
+  // Always called - converts event to relative position
+  function _basic(e) {
+    return {
+      x: e.clientX - deltaX,
+      y: e.clientY - deltaY
+    }
+  }
+
+  // Scales the point (if enabled)
+  function _scale(pos) {
+    pos.x *= scaleX;
+    pos.y *= scaleY;
+    return pos
+  }
+
+  // Transforms the point (if enabled) using the browsers' DOMMatrix
+  function _transforms(pos) {
+    var
+      currentMatrix = (context.currentTransform || context.mozCurrentTransform),
+      matrix,
+      imatrix;
+
+    // Convert from SVGMatrix (used by Chrome in experimental mode)
+    if (currentMatrix instanceof SVGMatrix) currentMatrix = DOMMatrix.fromMatrix(currentMatrix);
+
+    // Firefox returns Array instead of DOMMatrix..
+    matrix = typeof currentMatrix.a === "undefined" ? new DOMMatrix(currentMatrix) : currentMatrix;
+
+    // Eventually, get inverse matrix
+    imatrix = matrix.invertSelf();
+
+    return {
+      x: pos.x * imatrix.a + pos.y * imatrix.c + imatrix.e,
+      y: pos.x * imatrix.b + pos.y * imatrix.d + imatrix.f
+    }
+  }
+
+  // Transforms the point (if enabled) using the custom Matrix object
+  function _transforms2(pos) {
+    return imatrix.applyToPoint(pos.x, pos.y);
+  }
+
+  /*--------------------------------------------------------------------
+
+      PUBLIC METHODS
+
+  --------------------------------------------------------------------*/
+
+  /**
+   * Function to convert mouse or touch position to match the context
+   * and its element scale and transforms.
+   *
+   * @param {*} event - event object (mouse, touch) to use for conversion
+   * @returns {Point}
+   */
+  this.getPos = function(event) {
+    var pos = _basic(event);
+    if (doScale) pos = _scale(pos);
+    if (doTransforms) pos = transform(pos);
+    return pos
+  };
+
+  /**
+   * Pass in custom x and y point to convert to new position relative to
+   * bound context. The way it's calculated depends on the options set
+   * when creating the instance.
+   *
+   * The method can take both a Point object, or two numbers.
+   *
+   * @param {number|Point} x - x position to convert, or if Point the x and y of the object.
+   * @param {number} [y] - y position to convert (not used if Point object is provided).
+   * @returns {Point}
+   */
+  this.getPosXY = function(x, y) {
+    var _x; // to allow optimization (always a number)
+    if (arguments.length === 1) {
+      y = x.y;
+      _x = x.x;
+    }
+    else _x = x;
+
+    return me.getPos({
+      clientX: _x / scaleX + deltaX,
+      clientY: y / scaleY + deltaY
+    })
+  };
+
+  /**
+   * Force re-initialization. Use if canvas element have either border, padding
+   * or size changed.
+   * @method
+   */
+  this.init = init;
+
+  /**
+   * Used to manually update relative position in client if not caught
+   * automatically by internals (scroll and resize).
+   * @method
+   */
+  this.update = _updateOnScroll;
+
+  /*--------------------------------------------------------------------
+
+      PUBLIC PROPERTIES
+
+  --------------------------------------------------------------------*/
+
+  /**
+   * Set or get handleScale status. Can be toggled at any time and will
+   * affect next position calculations.
+   * @member {boolean} CanvasMouse#handleScale
+   */
+  defProp(me, "handleScale", {
+    get: function() {return doScale},
+    set: function(state) {doScale = !!state}
+  });
+
+  /**
+   * Set or get handleTransforms status. Can be toggled at any time and will
+   * affect next position calculations. **Note** that if the browser does not
+   * support currentTransform and a custom Matrix is not provided the flag
+   * will be set to false regardless.
+   * @member {boolean} CanvasMouse#handleTransforms
+   */
+  defProp(me, "handleTransforms", {
+    get: function() {return doTransforms},
+    set: function(state) {
+      doTransforms = !!state && (hasCurrentTransform || matrix);
+    }
+  });
+
+  /**
+   * Set or get custom matrix object. If set will override and patch the
+   * provided matrix.
+   * @member {Matrix} CanvasMouse#matrix
+   */
+  defProp(me, "matrix", {
+    get: function() {return matrix},
+    set: function(newMatrix) {
+      matrix = newMatrix;
+      doTransforms = doTransforms && (hasCurrentTransform || matrix);
+      patchMatrix();
+    }
+  });
+
+  /*--------------------------------------------------------------------
+
+      INITIALIZE INSTANCE
+
+  --------------------------------------------------------------------*/
+
+  patchMatrix();
+  init();
+
+  window.addEventListener("scroll", _handler);
+  window.addEventListener("resize", _handler);
+}
 
 /**
  * Valid properties for the option object passed to CanvasMouse constructor.
@@ -28,173 +281,3 @@
  * @prop {number} x - floating point number for x position
  * @prop {number} y - floating point number for y position
  */
-
-/**
- * A mouse and touch position handler for 2D canvas able to handle scaled
- * and transformed context and element.
- *
- * @param {CanvasRenderingContext2D} context - 2D context to bind to this instance
- * @param {CanvasMouseOptions} [options] - optional options object to allow for scale and transforms to be considered.
- * @constructor
- */
-function CanvasMouse(context, options) {
-
-  if (!(context instanceof CanvasRenderingContext2D))
-    throw "Need a 2D canvas context.";
-
-  options = Object.assign({
-    handleScale: false,
-    handleTransforms: false,
-    matrix: null
-  }, options);
-
-  var
-    me = this,
-    ref,
-    hasCurrentTransform = ("currentTransform" in context || "mozCurrentTransform" in context),
-    deltaX,
-    deltaY,
-    scaleX,
-    scaleY,
-    paddingBorderLeft,
-    paddingBorderTop,
-    canvas = context.canvas,
-    matrix = options.matrix,
-    imatrix = matrix ? matrix.inverse() : null,
-    doScale = options.handleScale,
-    doTransforms = options.handleTransforms,
-    transform = matrix ? _transforms2 : _transforms;
-
-  if (doTransforms && !hasCurrentTransform && !matrix) {
-    console.log("Browser doesn't support currentTransform.");
-    doTransforms = false;
-  }
-
-  // optimize inverse matrix extraction
-  if (doTransforms && matrix) {
-    matrix._xx = matrix._x;
-    matrix._x = function() {
-      imatrix = this.inverse();
-      return this._xx();
-    }
-  }
-
-  // Limit reflow so only read trigger values when absolutely needed
-  function init() {
-    var
-      rect = canvas.getBoundingClientRect(),
-      cs = getComputedStyle(canvas),
-      prop = cs.getPropertyValue.bind(cs),
-      pInt = parseInt,
-      _p = "padding-",
-      _b = "border-",
-      _w = "-width",
-      _pl = pInt(prop(_p + "left")),
-      _pr = pInt(prop(_p + "right")),
-      _pt = pInt(prop(_p + "top")),
-      _pb = pInt(prop(_p + "bottom")),
-      _bl = pInt(prop(_b + "left" + _w)),
-      _br = pInt(prop(_b + "right" + _w)),
-      _bt = pInt(prop(_b + "top" + _w)),
-      _bb = pInt(prop(_b + "bottom" + _w));
-
-    paddingBorderLeft = _pl + _bl;
-    paddingBorderTop = _pt + _bt;
-    deltaX = rect.left + paddingBorderLeft;
-    deltaY = rect.top + paddingBorderTop;
-    scaleX = (canvas.width / (rect.width - paddingBorderLeft - _pr - _br)) || 1;
-    scaleY = (canvas.height / (rect.height - paddingBorderTop - _pb - _bb)) || 1;
-  }
-
-  // debounce updates
-  function _handler() {
-    cancelAnimationFrame(ref);
-    ref = requestAnimationFrame(_updateOnScroll)
-  }
-
-  function _updateOnScroll() {
-    var rect = canvas.getBoundingClientRect();
-    deltaX = rect.left + paddingBorderLeft;
-    deltaY = rect.top + paddingBorderTop;
-  }
-
-  function _basic(e) {
-    return {
-      x: e.clientX - deltaX,
-      y: e.clientY - deltaY
-    }
-  }
-
-  function _scale(pos) {
-    pos.x *= scaleX;
-    pos.y *= scaleY;
-    return pos
-  }
-
-  function _transforms(pos) {
-    var matrix, cmatrix, imatrix, ctx = context;
-
-    cmatrix = (ctx.currentTransform || ctx.mozCurrentTransform);
-
-    // Convert from SVGMatrix (used by Chrome in experimental mode)
-    if (cmatrix instanceof SVGMatrix) cmatrix = DOMMatrix.fromMatrix(cmatrix);
-
-    matrix = typeof cmatrix.a === "undefined" ? new DOMMatrix(cmatrix) : cmatrix;
-    imatrix = matrix.invertSelf();
-
-    return {
-      x: pos.x * imatrix.a + pos.y * imatrix.c + imatrix.e,
-      y: pos.x * imatrix.b + pos.y * imatrix.d + imatrix.f
-    }
-  }
-
-  function _transforms2(pos) {
-    return imatrix.applyToPoint(pos.x, pos.y);
-  }
-
-  /**
-   * Function to convert mouse or touch position to match the context
-   * and its element scale and transforms.
-   *
-   * @param {*} e - event object (mouse, touch) to use for conversion
-   * @returns {Point}
-   */
-  this.getPos = function(e) {
-    var pos = _basic(e);
-    if (doScale) pos = _scale(pos);
-    if (doTransforms) pos = transform(pos);
-    return pos
-  };
-
-  /**
-   * Pass in custom x and y point to convert to new position relative to
-   * bound context.
-   *
-   * @param {number} x - x position to convert
-   * @param {number} y - y position to convert
-   * @returns {Point}
-   */
-  this.getPosXY = function(x, y) {
-    return me.getPos({
-      clientX: x / scaleX + deltaX,
-      clientY: y / scaleY + deltaY
-    })
-  };
-
-  /**
-   * Force re-initialization. Use if canvas element has border, padding
-   * or size changed.
-   */
-  this.init = init;
-
-  /**
-   * Used to manually update relative position in client if not caught
-   * automatically by internals.
-   */
-  this.update = _updateOnScroll;
-
-  init();
-
-  window.addEventListener("scroll", _handler);
-  window.addEventListener("resize", _handler);
-}
